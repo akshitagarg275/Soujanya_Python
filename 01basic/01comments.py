96e3f5647274dd1a7370ee3ae12e07e3b6f2a6e6
akshitagarg275 <akshitagarg275@gmail.com>
2022-07-14 08:54:37 +0530

    day 1

---
+++ 01basic/01comments.py
@@ -0,0 +1,12 @@
+#IT is a comment
+
+# print("Hello")
+
+'''
+It 
+is a 
+multiline
+comment
+'''
+
+print("Hey there")
---
+++ 01basic/03variables.py
@@ -0,0 +1,7 @@
+# variables -> these are the contianers used to store data
+
+num = 3
+
+print(num)
+
+print('num')
