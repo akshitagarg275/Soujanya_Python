17dde8fbe7b5e24c6a9e1f99625549881e167a69
akshitagarg275 <akshitagarg275@gmail.com>
2022-07-15 09:04:37 +0530

    day 2

--- 01basic/03variables.py
+++ 01basic/03variables.py
@@ -1,7 +1,47 @@
 # variables -> these are the contianers used to store data
 
-num = 3
+'''
+Rules to name a variable
+- never use a keyword as a variable name
+- a variable name consists of alphabets(A-Z , a-z) , digits(0,9),underscore(_)
+-spaces are not allowed
+- It is case-sensitive
+-Never start a variable name with a digit
 
-print(num)
+camel casing
+noOfProducts
+fullName
+marksOfStudent
 
-print('num')
+
+snake casing
+no_of_products
+full_name
+marks_of_student
+
+'''
+
+# num = 3
+
+# print(num)
+
+# print('num')
+
+# full_name = "John Doe"
+# print(full_name)
+
+# full_name = "Jane"
+# print(full_name)
+
+# TODO: constants -> always write constant in upper case
+
+BONUS = 4000
+print(BONUS)
+
+# BONUS = 9000
+# print(BONUS)
+
+
+
+
+
