47a8731b312d29284e93417662aab1f83dd697e8
akshitagarg275 <akshitagarg275@gmail.com>
2022-07-22 08:13:20 +0530

    day 4

---
+++ 01basic/04datatypes.py
@@ -0,0 +1,97 @@
+#TODO: complex 
+# real +/- imag number
+
+# c = 5 + 7j
+# print(c)
+# print(type(c))
+
+# TODO: Boolean True/False
+# is_valid = True
+# print(is_valid)
+# print(type(is_valid))
+
+# is_admin = False
+# print(is_admin)
+# print(type(is_admin))
+
+# TODO : strings
+# one line string '' , ""
+s = 'Hi, How are you'\
+    '?'
+# print (s)
+# print(type(s))
+
+msg = '''Hi
+We are writing 
+in multiline
+strings
+'''
+"""
+"""
+# print(msg)
+# print(type(msg))
+
+# s= "Let's play cricket"
+# print(s)
+# s= '"Python is a PL"'
+
+# print(s)
+
+# TODO: None
+# temp = None
+# print(temp)
+# print(type(temp))
+
+
+# TODO: Derived Datatypes
+# []
+# ()
+# {}
+# sequence datatypes -> list , tuple, dictionary, set,range
+
+# TODO: list []
+# it is heterogeneous
+
+# fruits = ['apple','mango','banana','orange']
+# print(fruits)
+# print(type(fruits))
+
+# TODO: tuple ()
+# fruits = ('apple','mango','banana','orange')
+# print(fruits)
+# print(type(fruits))
+
+# TODO: set {}
+# fruits = {'apple','mango','banana','orange'}
+# print(fruits)
+# print(type(fruits))
+
+# TODO: dictionary {key : value}
+
+stu = {
+    'name' :"John Doe",
+    'age' :15,
+    'courses': ['Python','Django']
+}
+
+# print(stu)
+# print(type(stu))
+
+# TODO: range 
+# range(start_index , end_index+1 , step)
+
+# nums = list(range(1, 6))
+# print(nums)
+# print(type(nums))
+
+# even_nums = tuple(range(0,11,2))
+# print(even_nums)
+# print(type(even_nums))
+
+rev = list(range(10,0,-1))
+print(rev)
+
+
+
+
+
---
+++ 01basic/05type_casting.py
@@ -0,0 +1,28 @@
+# TO explicitly change the datatype
+
+'''
+int()
+float()
+str()
+bool()
+complex()
+list()
+tuple()
+dict()
+set()
+'''
+
+# a = 45
+# b= float(a)
+# b = str(a)
+# print(a , type(a))
+# print(b , type(b))
+
+# s = int('a')
+# print(s , type(s))
+
+# name = list("John")
+# print(name , type(name))
+
+is_Admin = int(True)
+print(is_Admin , type(is_Admin))
