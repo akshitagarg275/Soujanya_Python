0311606572f922f032886254a156442ab17494d4
akshitagarg275 <akshitagarg275@gmail.com>
2022-07-22 08:55:19 +0530

    day 5

---
+++ 01basic/06input.py
@@ -0,0 +1,9 @@
+# input() -> by default input takes data as string
+
+# full_name = input("Enter your name: ")
+# print("Hi ", full_name)
+# print(type(full_name))
+
+# age = int(input("Enter your age: "))
+# print(age)
+# print(type(age))
---
+++ 01basic/07operators.py
@@ -0,0 +1,95 @@
+'''
+operators -> used to perform operations
+
+unary -> which requires one operand
+    minus sign
+binary -> It requires two operands
+    arithmetic
+    assignment
+    relational
+    logical
+    bitwise
+    membership
+    identity
+'''
+
+# TODO: unary
+# a = -4
+
+# TODO: ARITHMETIC
+# print(5+4)
+# print(5-4)
+# print(5*4)
+# print(5/4)
+
+# NOTE: Floor division (//)
+# print(5//4)
+
+# NOTE: Modulus operator(%)
+# print(5%4)
+
+# NOTE: Exponentation (**)
+# print(5 ** 2)
+
+# TODO: Assignment (=)
+# a = 2
+
+# exp = ((2+5) -((4 /3)*2))
+# print(exp)
+
+# NOTE: Shorthand Notation
+# a = a + 3
+# a+= 3
+
+
+# TODO: Relational(comparison) Operator
+# NOTE: It return boolean value (True/False)
+'''
+> - greater than
+< - less than
+>= - greater than equal to
+NOTE: LHS literal value is either greater(less) than or equal to RHS literal value
+<= - less than equal to
+== - equality
+!= - not equal to
+
+'''
+
+# print(5>3>1)
+# print(5<2)
+
+# print(5<=5)
+
+# print(5>=2)
+
+# print(5==5)
+# print(5!=5)
+
+
+# TODO: Logical Operator
+'''
+and -> If any one i/p is False o/p will be False
+
+or -> If anyone i/p is True o/p will be True
+
+not -> It reverses the state of the input
+
+'''
+
+# print(True and True)
+# print(True and False)
+
+# print(True or True)
+# print(True or False)
+
+
+is_email = False
+is_google = False
+is_facebook = True
+
+# signedIn = is_email or is_facebook or is_google
+
+# print("Are uou signed in? ",signedIn)
+
+# Falsy values (False, None , '' , 0)
+print(not 5)
